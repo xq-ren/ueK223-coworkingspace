7bf9939155c8789a0002b54176893ea5f08406c2
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2022-11-23 14:23:52 +0100

    updated essential files

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,14 +7,14 @@ VALUES ('2', 'MITGLIED');
 INSERT INTO Role (id, roleName)
 VALUES ('3', 'BESUCHER');
 
-INSERT INTO AppUser (id, username, password, role, entries)
-VALUES ('1', 'tpiana', '123wert456', 'ADMIN', '80');
+INSERT INTO AppUser (id, username, password, role_id, entries)
+VALUES ('1', 'tpiana', '123wert456', '1', '80');
 
-INSERT INTO AppUser (id, username, password, role, entries)
-VALUES ('5', 'jsingh', '876iuz543', 'MITGLIED', '2' '4');
+INSERT INTO AppUser (id, username, password, role_id, entries)
+VALUES ('5', 'jsingh', '876iuz543', '2', '2' '4');
 
-INSERT INTO AppUser (id, username, password, role, entries)
-VALUES ('10', 'jvall', 'dfgert345', 'BESUCHER', '');
+INSERT INTO AppUser (id, username, password, role_id, entries)
+VALUES ('10', 'jvall', 'dfgert345', '3', '');
 
 INSERT INTO Entry (id, bookingTime, bookingText, bookingStatus)
 VALUES ('4', '14:00', 'Buchung für User jsingh', 'PROCESSING');
