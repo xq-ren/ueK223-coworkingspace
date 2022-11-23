34d7b33f857a8dc2fa1c0c73ec1957f4ecf007fb
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2022-11-23 15:26:38 +0100

    updated authcontroller and added tests about authentication

--- src/main/java/ch/zli/m223/controller/AuthController.java
+++ src/main/java/ch/zli/m223/controller/AuthController.java
@@ -10,17 +10,33 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.AppUser;
+import ch.zli.m223.service.AppUserService;
 import ch.zli.m223.service.AuthService;
 
 @Path("/session")
 @Tag(name = "Session", description = "Handling of sessions")
-@RolesAllowed({ "User", "Admin" })
 public class AuthController {
   
   @Inject
   AuthService authService;
 
+  @Inject
+  AppUserService appUserService;
+
   @POST
+  @Path("/register")
+  @PermitAll
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(summary = "create user", description = "create user")
+  public AppUser registerUser(AppUser appUser) {
+    return appUserService.createUser(appUser);
+  }
+
+  @POST
+  @Path("/login")
+  @RolesAllowed({ "User", "Admin" })
   @Consumes(MediaType.APPLICATION_JSON)
   @Operation(summary = "Authenticate a user.", description = "Returns a token upon successful authentication.")
   public String authenticate(@QueryParam("username")String username, @QueryParam("passwort") String passwort) {
